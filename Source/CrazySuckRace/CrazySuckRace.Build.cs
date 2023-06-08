b33752fabfa1ebaa655be942613b93a37c6de941
Louis-GRANGE <46597365+Louis-GRANGE@users.noreply.github.com>
2023-06-08 20:05:47 +0200

    [ADD] Steam Session

---
+++ Source/CrazySuckRace/CrazySuckRace.Build.cs
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class CrazySuckRace : ModuleRules
+{
+	public CrazySuckRace(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		// DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		PrivateDependencyModuleNames.AddRange(new string[] {"OnlineSubsystem", "OnlineSubsystemNull", "OnlineSubsystemSteam" });
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
